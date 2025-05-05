15a945ecdc1ef185aa0eb88967e9b16004c4e412
DavI Lindão <davievangelistasampaio@gmail.com>
2025-05-05 11:54:25 +0000

    exemplos 5 e 6

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
  Exemplo1
  function mostrarAlerta(){
-     let nome = "Davi Lindão"
+     let nome = "Pedro"
      alert("Seja bem-vindo(a), "+nome)
  }
  
@@ -18,14 +18,14 @@
  
  somar(20,12)
  */
-/*
+ /*
  Exemplo 3
  function mensagemBoasVindas(nome, cargo){
      alert(`Seja bem-vindo(a),  ${cargo} ${nome}`)
  
  }
  
- mensagemBoasVindas("Davi Lindão" ,"Poderoso chefão")
+ mensagemBoasVindas("Pedro" ,"Espetacular")
  */
  
  
@@ -42,3 +42,30 @@
  console.log(multiplicacao2)
  console.log(multiplicacao3)
  */
+ /* Exemplo 5
+ function parOuImpar(numero){
+     if(numero %2 === 0){
+         return "Par"
+     }
+ 
+     return "Ímpar"
+ }
+ 
+ alert(parOuImpar(5))
+ */
+ /* Exemplo 6
+ 
+ let numero = 10
+ 
+ function aumentarNumero(){
+     numero += 1
+     console.log(numero)
+  }
+ 
+ 
+ const button = document.querySelector("button")
+ button.addEventListener("click", aumentarNumero)
+ 
+ console.log(numero)
+ */
+ 
